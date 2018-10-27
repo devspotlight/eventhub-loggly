f876e651afb9d03ca7da239d5a1ea5550d59a599
AlverezYari <caseyphillips@gmail.com>
2018-10-27 14:23:27 -0500

    Clean up code after testing on Azure

--- ehloggymain/__init__.py
+++ ehloggymain/__init__.py
@@ -1,14 +1,12 @@
 import json 
 import azure.functions as func
-import logging
 import requests
 
 
 def main(event):
-    test = event.get_body()
-    test2 = test.decode("utf-8")
-    test3 = json.dumps(test2)
-    r = requests.post('http://logs-01.loggly.com/inputs/ff2337d9-dd24-4030-8c6e-309ae53738db/tag/http/', data = test3)
+    package = event.get_body()
+    package = json.dumps(package.decode("utf-8"))
+    r = requests.post('http://logs-01.loggly.com/inputs/ff2337d9-dd24-4030-8c6e-309ae53738db/tag/ehlog/', data = package)
     return 
 
    
