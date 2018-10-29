abab57e3f9b6a0a0f389b481021961ee6cf1c807
AlverezYari <caseyphillips@gmail.com>
2018-10-29 14:03:19 -0500

    fixing folders

--- ehlogglymain/__init__.py
+++ ehlogglymain/__init__.py
@@ -4,9 +4,10 @@ import requests
 
 
 def main(event):
+    logglyendpoint = "<Loggly HTTP Endpoint with tag goes here>"
     package = event.get_body()
     package = json.dumps(package.decode("utf-8"))
-    r = requests.post('http://logs-01.loggly.com/inputs/ff2337d9-dd24-4030-8c6e-309ae53738db/tag/ehlog/', data = package)
+    r = requests.post(logglyendpoint, data = package)
     return 
 
    
