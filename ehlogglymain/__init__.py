7d929c8bc13354e44c541ab8e6a6fedfbe6b2021
Casey Phillips <caseyphillips@gmail.com>
2019-09-06 13:20:54 -0500

    Fixed json loading

--- ehlogglymain/__init__.py
+++ ehlogglymain/__init__.py
@@ -7,7 +7,7 @@ def main(event):
 
     logglyendpoint = "<Loggly HTTP Endpoint with tag goes here>"
     package = event.get_body()
-    package = json.loads(package.decode("utf-8"))
+    package = json.dumps(package.decode("utf-8"))
     r = requests.post(logglyendpoint, json=package)
     return 
 
